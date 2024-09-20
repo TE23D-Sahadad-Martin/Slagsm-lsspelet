6e6106d5ebbc4652307894eec3c8dff1458c329c
martin sahadad <martin.sahadad@elev.ga.ntig.se>
2024-09-20 10:41:32 +0200

    uyfkuyf

---
+++ Slagsmålsspelet/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true) 
+{
+int card = Random.Shared.Next(52);
+Console.WriteLine(card);
+Console.ReadLine(); 
+}
+
+
+
+
+Console.WriteLine("Martin");
+
+string name = Console.ReadLine(4e4); 
+while (name.Length < 3)
+{
+    Console.WriteLine("Martin, sa jag dräng");
+    name = Console.ReadLine(); 
+}
+
+Console.ReadLine(); 
